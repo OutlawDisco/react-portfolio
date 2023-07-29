901df28658c27f7b83c60ef9d70237fcf764e8d0
OutlawDisco <aoutlaw00@gmail.com>
2023-07-29 10:37:07 -0600

    installed packages, renamed pages and navtabs

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import NavTabs from './NavTabs';
-import Home from './pages/Home';
-import About from './pages/About';
-import Blog from './pages/Blog';
-import Contact from './pages/Contact';
+import React, { useState } from "react";
+import NavTabs from "./NavTabs";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
+import Contact from "./pages/Contact";
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState("Home");
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
+    if (currentPage === "Home") {
       return <Home />;
     }
-    if (currentPage === 'About') {
-      return <About />;
+    if (currentPage === "Projects") {
+      return <Projects />;
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
+    if (currentPage === "Experience") {
+      return <Experience />;
     }
     return <Contact />;
   };
