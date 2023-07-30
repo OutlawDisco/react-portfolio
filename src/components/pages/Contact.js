eb3157828412139b18494f903f828983dc6a3761
OutlawDisco <aoutlaw00@gmail.com>
2023-07-30 15:33:12 -0600

    Imoorted mui icons

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,17 @@
 import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailIcon from "@mui/icons-material/Email";
 
 export default function Contact() {
   return (
     <div>
       <h1>Contact Me</h1>
+      <div className="socials">
+        <LinkedInIcon href="" />
+        <GitHubIcon href="https://github.com/OutlawDisco" />
+        <EmailIcon href="" />
+      </div>
     </div>
   );
 }
