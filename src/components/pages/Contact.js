3be8dcf290ba467ed45e89ef141892a527451337
OutlawDisco <aoutlaw00@gmail.com>
2023-07-31 20:06:51 -0600

    wrapped links in a tag

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,10 +7,22 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact Me</h1>
-      <div className="socials">
-        <LinkedInIcon href="" />
-        <GitHubIcon href="https://github.com/OutlawDisco" />
-        <EmailIcon href="" />
+      <div className="row">
+        <div class="socials d-flex justify-content-center">
+          <a href="" target="_blank" rel="noreferrer">
+            <LinkedInIcon sx={{ fontSize: 70 }} />
+          </a>
+          <a
+            href="https://github.com/OutlawDisco"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <GitHubIcon sx={{ fontSize: 70 }} />
+          </a>
+          <a href="" target="_blank" rel="noreferrer">
+            <EmailIcon sx={{ fontSize: 70 }} />
+          </a>
+        </div>
       </div>
     </div>
   );
