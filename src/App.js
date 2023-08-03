824c9df4332a398a76f6e11eec46a767a1c2158f
OutlawDisco <aoutlaw00@gmail.com>
2023-08-03 09:51:33 -0600

    Updates

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import PortfolioContainer from "./components/PortfolioContainer";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-
-function App() {
-  return <PortfolioContainer />;
-}
-
-export default App;
