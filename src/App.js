a205dacb00181544c2c17d54648f5651b2e3cf60
OutlawDisco <aoutlaw00@gmail.com>
2023-08-03 09:12:33 -0600

    recloned repo

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import PortfolioContainer from "./components/PortfolioContainer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return <PortfolioContainer />;
+}
+
+export default App;
---
+++ src/components/NavTabs.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+// Here we are using object destructuring assignment to pluck off our variables from the props object
+// We assign them to their own variable names
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <Nav defaultActiveKey="/home" className="flex-column">
+      <Nav.Link
+        href="/home"
+        onClick={() => handlePageChange("Home")}
+        className={currentPage === "Home" ? "nav-link active" : "nav-link"}
+      >
+        About Me
+      </Nav.Link>
+      <Nav.Link
+        eventKey="projects"
+        onClick={() => handlePageChange("Projects")}
+        className={currentPage === "Projects" ? "nav-link active" : "nav-link"}
+      >
+        Portfolio
+      </Nav.Link>
+      <Nav.Link
+        eventKey="experience"
+        onClick={() => handlePageChange("Experience")}
+        className={
+          currentPage === "Experience" ? "nav-link active" : "nav-link"
+        }
+      >
+        Resume
+      </Nav.Link>
+      <Nav.Link
+        eventKey="contact"
+        onClick={() => handlePageChange("Contact")}
+        className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
+      >
+        Contact
+      </Nav.Link>
+    </Nav>
+  );
+}
+
+export default NavTabs;
