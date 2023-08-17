704dca12634a64e1c400d50803d1c0aac287226c
OutlawDisco <aoutlaw00@gmail.com>
2023-08-17 11:29:15 -0600

    Updated

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,17 +1,19 @@
-import React from "react";
-import Figure from "react-bootstrap/Figure";
-import headshot from "../../Images/Headshot.jpg";
+import React from 'react';
+import Figure from 'react-bootstrap/Figure';
+import headshot from '../../Images/Headshot.jpg';
 
 export default function Home() {
   return (
     <div>
       <h1>Angela Outlaw-Lyons</h1>
-      <p>Full-stack developer & artist</p>
+      <p>Full-stack Web Developer</p>
       <div class="d-flex justify-content-center">
         <Figure.Image width={271} height={280} alt="headshot" src={headshot} />
       </div>
       <p class="skills">
-        I've worked with: Node.js, Express.js, SQL, NoSQL, MongoDB, React
+        Recent graduate of the University of Denver coding bootcamp. Technical
+        skills in HTML, CSS, JavaScript, OOP & MVC, MySQL, MongoDB, Node.js,
+        Express.js and React
       </p>
     </div>
   );
